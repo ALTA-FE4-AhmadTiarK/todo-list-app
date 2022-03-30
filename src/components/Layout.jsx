870942bb8a7666f3c6d98f9ee3e50e7d1ca664a9
ATiarK <97088690+ATiarK@users.noreply.github.com>
2022-03-30 22:25:40 +0700

    clean unused code

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,11 +3,7 @@ import React from 'react';
 const Layout = (props) => {
 	return (
 		<div className='layout'>
-			<div className='container'>
-				<div className='row'>
-					<div className='col-md-12'>{props.children}</div>
-				</div>
-			</div>
+			<div className='container my-5'>{props.children}</div>
 		</div>
 	);
 };
--- src/pages/App.js
+++ src/pages/App.js
@@ -12,7 +12,6 @@ function App() {
 		<div className='App'>
 			<Navbar title='Todo Apps' />
 			<Layout>
-				<h1>New Todo App</h1>
 				<Form
 					inputText={inputText}
 					setInputText={setInputText}
