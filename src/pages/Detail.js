9638e09ccea77b304a88e486ce3975a3a096966d
ATiarK <97088690+ATiarK@users.noreply.github.com>
2022-03-30 22:24:53 +0700

    add detail page

---
+++ src/pages/Detail.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Layout from '../components/Layout';
+import Navbar from '../components/Navbar';
+
+const Detail = () => {
+	return (
+		<div>
+			<Navbar title='Detail Todo' />
+			<Layout>
+				<h1>Detail To-Do</h1>
+			</Layout>
+		</div>
+	);
+};
+
+export default Detail;
