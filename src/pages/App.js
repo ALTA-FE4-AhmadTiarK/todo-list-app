232e474829c892fb2591ad8a0779e4f448100f56
ATiarK <97088690+ATiarK@users.noreply.github.com>
2022-03-30 11:31:01 +0700

    update files

---
+++ src/pages/App.js
@@ -0,0 +1,15 @@
+import logo from '../assets/logo.svg';
+import '../styles/App.css';
+
+function App() {
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<img src={logo} className='App-logo' alt='logo' />
+				<h1>New Todo App</h1>
+			</header>
+		</div>
+	);
+}
+
+export default App;
