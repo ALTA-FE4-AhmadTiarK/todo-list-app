64ca218f08134b5076815682e95afde00edb0332
ATiarK <97088690+ATiarK@users.noreply.github.com>
2022-03-31 11:55:04 +0700

    tried to fetch data

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,16 +1,36 @@
-import { useState } from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Form from '../components/Form';
 import Layout from '../components/Layout';
 import Navbar from '../components/Navbar';
 import TodoList from '../components/TodoList';
 import '../styles/App.css';
+import { withRouter } from '../utils/navigation';
 
 function App() {
 	const [inputText, setInputText] = useState('');
 	const [todos, setTodos] = useState([]);
+
+	useEffect(() => {
+		fetchData();
+	}, []);
+
+	const fetchData = async () => {
+		await axios
+			.get(
+				`https://api.todoist.com/rest/v1/projects?Authorization=Bearer&token=${process.env.REACT_APP_API_KEY}`
+			)
+			.then((response) => {
+				console.log(response.data);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
+
 	return (
 		<div className='App'>
-			<Navbar title='Todo Apps' />
+			<Navbar title='Todo Apps' btnText='Detail' />
 			<Layout>
 				<Form
 					inputText={inputText}
@@ -24,4 +44,4 @@ function App() {
 	);
 }
 
-export default App;
+export default withRouter(App);
