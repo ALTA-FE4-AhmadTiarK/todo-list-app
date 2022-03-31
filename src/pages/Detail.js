51a5d474442bad21d172e94f68a796e240bc782e
ATiarK <97088690+ATiarK@users.noreply.github.com>
2022-03-31 11:54:48 +0700

    add some card

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Layout from '../components/Layout';
 import Navbar from '../components/Navbar';
 
 const Detail = () => {
 	return (
 		<div>
-			<Navbar title='Detail Todo' />
+			<Navbar title='Detail Todo' btnText='Home' />
 			<Layout>
-				<h1>Detail To-Do</h1>
+				<h1 className='text-center my-5'>Detail To-Do</h1>
+				<div className='card'>
+					<div className='card-header'>Task List</div>
+				</div>
 			</Layout>
 		</div>
 	);
