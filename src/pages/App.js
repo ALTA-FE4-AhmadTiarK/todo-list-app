3268407f4123fb1bdef211eb1b86e0fbcde85467
ATiarK <97088690+ATiarK@users.noreply.github.com>
2022-03-31 17:17:26 +0700

    add & fetch data from todoist

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,4 +1,3 @@
-import axios from 'axios';
 import { useEffect, useState } from 'react';
 import Form from '../components/Form';
 import Layout from '../components/Layout';
@@ -10,23 +9,6 @@ import { withRouter } from '../utils/navigation';
 function App() {
 	const [inputText, setInputText] = useState('');
 	const [todos, setTodos] = useState([]);
-
-	useEffect(() => {
-		fetchData();
-	}, []);
-
-	const fetchData = async () => {
-		await axios
-			.get(
-				`https://api.todoist.com/rest/v1/projects?Authorization=Bearer&token=${process.env.REACT_APP_API_KEY}`
-			)
-			.then((response) => {
-				console.log(response.data);
-			})
-			.catch((err) => {
-				console.log(err);
-			});
-	};
 
 	return (
 		<div className='App'>
